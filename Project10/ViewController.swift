56841e3f850d4dc8a10e11abddd62bd3efdf3c32
o-pylypiv <bassenia@gmail.com>
2024-03-11 15:13:36 +0200

    Initial Commit

---
+++ Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Olha Pylypiv on 11.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
